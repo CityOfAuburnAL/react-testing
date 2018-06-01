30822ca24d4eef02d1b3829d456ba63e1dc34810
Chris Griffin <chris.sgriffin@gmail.com>
2018-06-01 11:25:28 -0500

    Fixed router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './layouts/App/app';
 import './assets/css/index.css';
 
 const rootElement = document.body.querySelector('#root');
 if (rootElement) {
-	render(<BrowserRouter><App /></BrowserRouter>, rootElement)
+	render(<HashRouter><App /></HashRouter>, rootElement)
 }
